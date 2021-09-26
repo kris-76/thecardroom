6c5f3f19f1e5833ac2f5ad3d540623bf7273fce9
Kris Henderson <kris.henderson76@gmail.com>
2021-09-26 10:23:22 +0700

    Set generic username

--- scripts/mainnet/run-db-sync.sh
+++ scripts/mainnet/run-db-sync.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
-
-sudo service postgresql start > db_sync_log.txt
+export MAINNET_CARDANO_NODE_SOCKET_PATH=/home/ubuntu/cardano/mainnet/db/node.socket
+export CARDANO_NODE_SOCKET_PATH=$MAINNET_CARDANO_BNODE_SOCKET_PATH
 PGPASSFILE=../../cardano-src/cardano-db-sync/config/pgpass-mainnet ../../cardano-src/cardano-db-sync/db-sync-node/bin/cardano-db-sync --config ../../cardano-src/cardano-db-sync/config/mainnet-config.yaml --socket-path $MAINNET_CARDANO_NODE_SOCKET_PATH --state-dir ../../cardano-src/cardano-db-sync/ledger-state/mainnet --schema-dir ../../cardano-src/cardano-db-sync/schema/ >> db_sync_log.txt 2>&1 &
--- scripts/mainnet/run-node.sh
+++ scripts/mainnet/run-node.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
-export MAINNET_CARDANO_NODE_SOCKET_PATH=/home/krish/cardano/mainnet/db/node.socket
+export MAINNET_CARDANO_NODE_SOCKET_PATH=/home/ubuntu/cardano/mainnet/db/node.socket
 export CARDANO_NODE_SOCKET_PATH=$MAINNET_CARDANO_BNODE_SOCKET_PATH
-cardano-node run --config /home/krish/cardano/mainnet/mainnet-config.json --database-path /home/krish/cardano/mainnet/db/ --socket-path $MAINNET_CARDANO_NODE_SOCKET_PATH --host-addr 127.0.0.1 --port 2338 --topology /home/krish/cardano/mainnet/mainnet-topology.json > node_log.txt 2>&1 &
+cardano-node run --config /home/ubuntu/cardano/mainnet/mainnet-config.json --database-path /home/ubuntu/cardano/mainnet/db/ --socket-path $MAINNET_CARDANO_NODE_SOCKET_PATH --host-addr 127.0.0.1 --port 2338 --topology /home/ubuntu/cardano/mainnet/mainnet-topology.json > node_log.txt 2>&1 &
