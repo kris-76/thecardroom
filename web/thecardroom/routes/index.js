5e96422295cfe8d450cd30e22ed820279dbeb2d3
Kris Henderson <kris.henderson76@gmail.com>
2022-04-02 16:57:23 -0700

    cleanup styles and menu.  add about.

--- web/thecardroom/routes/index.js
+++ web/thecardroom/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'The Card Room' });
 });
 
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'The Card Room | About' });
+});
+
 module.exports = router;
